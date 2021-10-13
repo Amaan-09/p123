32c2ba4471687ec1af722dea2edb9b4dd78df750
Amaan-09 <79093439+Amaan-09@users.noreply.github.com>
2021-10-13 19:24:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,44 @@
-function setup()
-{
-    video= createCapture(VIDEO);
-    video.size(550,500);
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 
-    canvas= createCanvas(550,550);
-    canvas.position(560,150);
-
-    poseNet= ml5.poseNet(video, modelLoaded);
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+  
+    canvas = createCanvas(550, 550);
+    canvas.position(560,250);
+  
+    poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
-}
-
-function modelLoaded()
-{
-    console.log('PoseNet is Initialized!');
-}
+  }
+  
+  function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+  }
 
 function gotPoses(results)
 {
-if(results.length > 0){
-    console.log(results);
+    if(results.length > 0)
+    {
+        console.log(results);
+
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX);
+    }
 }
+
+
+function draw()
+{
+    background('#6495ed');
+
+    document.getElementById("font-size-id").innerHTML = "Font Size of the text will be = " + difference +"px";
+    textSize(difference);
+    fill('#87CEEB');
+    text('Amaan', 50, 400);
 }
